54ddcb89f9029cc31f5728345a5e65042cefb312
get <>
2019-01-06 16:41:55 -0700

    upgrate

--- server/routes/api/posts-gcp.js
+++ server/routes/api/posts-gcp.js
@@ -8,12 +8,10 @@ secondRouter.get('/', async (req, res) => {
     res.send(await posts.find({}).toArray());
 });
 
-
-secondRouter.get('/about', function (req, res) {
-    console.log('about open');
+secondRouter.get('/about', async (req, res) => {
     const posts = await loadPostsCollection(); 
     res.send(await posts.find({}).toArray());
-})
+});
 
 // ADD POST
 secondRouter.post('/', async (req, res) => {
